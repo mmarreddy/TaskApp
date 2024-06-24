826265a6ada3c4022f8577e334a3f02b1ff856e8
Mounika Marreddy <mouni@Mounika.local>
2024-06-24 09:34:46 +0530

    addedCustomHook

--- task manager/src/components/Task.js
+++ task manager/src/components/Task.js
@@ -3,41 +3,41 @@ import { useState } from 'react';
 import Modal from './Modal';
 import './task.css'
 import UserContext from './UserContext';
+import useInput from './useInput';
 
 const Task = () => {
     const {user} = useContext(UserContext)
-    const [name, setName] = useState('');
-    const[description, setDescription] = useState('');
-    const[difficulty, setDifficulty] = useState('');
+    const name = useInput('');
+    const description = useInput('');
+    const difficulty = useInput('');
     const [taskCounter, setTaskCounter] = useState(0);
     const [modalOpen, setModalOpen] = useState(false);
     const [taskList, setTaskList] = useState([]);
+
     const handleClose = () => {
       setModalOpen(false);
   };
 
-  useEffect = (() =>{
-    handleClose()
-  }, [])
+    useEffect(() => {
+      if (taskCounter > 0) {
+          setModalOpen(true);
+      }
+  }, [taskCounter]);
 
-
-  // useEffect(()=>{
-  //   setTimeout(() => {
-  //     setModalOpen(false);
-  //   }, 3000);
-  // }, [])
+  useEffect(()=>{
+    if (modalOpen){
+      setTimeout(()=>{
+        handleClose();
+      }, 3000);
+    }
+  })
 
     function handleSubmit (e) {
         e.preventDefault();
         setTaskCounter (prev => prev + 1);
-        setModalOpen(true)
-        setTaskList([...taskList, {name: name, description:description, difficulty: difficulty} ])
+        setTaskList([...taskList, {name: name.value, description:description.value, difficulty: difficulty.value} ])
     }
   
-  //   useEffect(() => {
-  //     setModalOpen(true)
-  // }, [taskCounter]); 
-
   return (
     <div className='task-container'>   
    
@@ -46,11 +46,11 @@ const Task = () => {
       <form className="form" onSubmit={handleSubmit} style={{display: "grid", maxWidth: "auto", gap: "20px", justifyContent:"center"}}>
       
         <label htmlFor="name">Enter task name:</label>
-        <input type="textbox" placeholder="task" id="name" value={name} onChange={(e) => setName(e.target.value)}required/>
+        <input type="textbox" placeholder="task" id="name" {...name} required/>
         <label htmlFor="description">Enter task description:</label>
-        <input type="textbox" placeholder="description" id="description" value={description} onChange={(e) => setDescription(e.target.value)}required/>
+        <input type="textbox" placeholder="description" id="description" {...description} required/>
         <label htmlFor="difficulty">Enter the difficulty:</label>
-        <input type="number" placeholder="1" min="1" max="5" id="difficulty" value={difficulty} onChange={(e) => setDifficulty(e.target.value)}required/>
+        <input type="number" placeholder="1" min="1" max="5" id="difficulty" {...difficulty} required/>
         <button className='button' type="submit">Add Task</button>
         <Modal isOpen={modalOpen} onClose={handleClose}></Modal>
       </form>
